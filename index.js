af55db2466db2cb8b4903a0644385b0e9ad60d45
cicciotazza <cicciotazza@hotmail.com>
2022-01-13 17:24:12 +0100

    Data Security, Validation & Ethics
    added Heroku

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const uuid = require("uuid");
 const app = express();
-//const { check, validationResult } = require("express-validator");
+
 //integration with a REST API, requiring Mongoose/Models and access to movies/users/genres/directos
 const mongoose = require("mongoose");
 const Models = require("./models.js");
+
+//server side validation
+const { check, validationResult } = require("express-validator");
 
 const Movies = Models.Movie;
 const Users = Models.User;
@@ -28,11 +31,29 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(morgan("common"));
 
+//Import CORS (Cross-Origin-Resource-Sharing)
+const cors = require("cors");
+app.use(cors());
+//app.use(cors()); to substitute for an list of allowed domains within the variable allowedOrigins
+/*
+let allowedOrigins ["http://localhost:8080", "htpp://testsite.com"];
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      //If a specific origin isn’t found on the list of allowed origins
+      let message ="The CORS policy for this application doesn't allow access from origin " + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
+*/
+
 //Import "Auth.js" file
 let auth = require("./auth")(app);
 const passport = require("passport");
 require("./passport");
-
 
 //GET req, welcome message as res
 app.get("/",
@@ -125,54 +146,48 @@ passport.authenticate("jwt", { session: false }),
   });
 });
 
-//NOT WORKING ALTERNATIVES
-/*app.get("/genre/:Name", (req, res) =>{
-  Genres.findOne({ Name: req.params.Name })
-    .then((genre) =>{
-      res.json(genre.Description);
-    })
-    .catch((err) =>{
-      console.error(error);
-      res.status(500).send("Error: " + err);
-    });
-});
-
-app.get("/directors/:Name", (req, res) =>{
-    myFlixDB.findOne({"Director.Name": req.params.Name})
-    .then((movie)=>{
-        res.json(movie.Director);
-    })
-    .catch((err)=>{
-        console.error(err);
-        res.status(500).send("Error" + err);
-    });
-});*/
-
-    //ADD a new user with JSON format
-    app.post("/users", (req, res) =>{
-      Users.findOne({ Username: req.body.Username })
-        .then((user) =>{
-          if (user) {
-            return res.status(400).send(req.body.Username + "already exists");
-          } else {
-            Users.create({
-                Username: req.body.Username,
-                Password: req.body.Password,
-                Email: req.body.Email,
-                Birthday: req.body.Birthday
-              })
-              .then((user) =>{res.status(201).json(user); })
-              .catch((error) =>{
-                console.error(error);
-                res.status(500).send("Error: " + error);
-              })
-          }
-        })
-        .catch((error) =>{
-          console.error(error);
-          res.status(500).send("Error: " + error);
+//ADD a new user with JSON format
+app.post("/users",
+       //validation logic for the request
+    [  
+    check("Username", "Username is required").isLength({min: 4}),
+    check("Username", "Username contains non alphanumeric characters - not allowed.").isAlphanumeric(),
+    check("Password", "Password is required").not().isEmpty(),
+    check("Email", "Email does not appear to be valid").isEmail()
+    ],
+(req, res) => {
+  // check the validation object for errors
+  let errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }  
+  //hash any password when registering before storing it on the MongoDB db
+  let hashedPassword = Users.hashedPassword(req.body.Password);
+  Users.findOne({ Username: req.body.Username })
+//Search if a user with a requested username already exists
+    .then((user) =>{
+      if (user) {
+        //if user found, sens a response that it already exists
+          return res.status(400).send(req.body.Username + "already exists");
+        } else {
+          Users.create({
+                  Username: req.body.Username,
+                  Password: req.body.Password,
+                  Email: req.body.Email,
+                  Birthday: req.body.Birthday
+                      })
+                .then((user) =>{res.status(201).json(user); })
+                .catch((error) =>{
+                  console.error(error);
+                  res.status(500).send("Error: " + error);
+                });
+              }
+            })
+            .catch((error) =>{
+              console.error(error);
+              res.status(500).send("Error: " + error);
+            });
         });
-    });
   /*    {
   ID: Integer,
   Username: String,
@@ -273,7 +288,7 @@ res.status(500).send("Error founded, fix it!");
 next();  
 });
 
-//Listens for requests, port 8080
-app.listen(8080, () =>{
-    console.log("This app is listening on port 8080.");
+const port = process.env.PORT || 8080;
+app.listen(port, "0.0.0.0",() => {
+  console.log("Listening on port " + port);
 });
