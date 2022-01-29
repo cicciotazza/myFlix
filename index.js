93f37fbae30579f0e1d9ce9bd4fcc6a0e8432688
cicciotazza <cicciotazza@hotmail.com>
2022-01-29 15:35:10 +0100

    2.10l allowed origins CORS <--> localhost:1234

--- index.js
+++ index.js
@@ -22,23 +22,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("common"));
 
+let allowedOrigins = ['http://localhost:8081', 'http://localhost:1234', 'https://myflix-by-jop.herokuapp.com', '*'];
+
 //import cors
 const cors = require('cors');
-app.use(cors());
-/* if you want only certain origins to be given access: list of allowed domains
-app.use(cors());
-let allowedOrigins ["http://localhost:8080", "htpp://testsite.com"];
 app.use(cors({
   origin: (origin, callback) => {
     if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      //If a specific origin isn’t found on the list of allowed origins
-      let message ="The CORS policy for this application doesn't allow access from origin " + origin;
+    if(allowedOrigins.indexOf(origin) === -1){ 
+    // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
       return callback(new Error(message ), false);
     }
     return callback(null, true);
   }
-}));        */
+}));
 
 //Import "Auth.js" file and Passport module
 let auth = require('./auth')(app);
