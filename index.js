f7513a983016c5b44a6bd01f5627bc447e1ef295
cicciotazza <cicciotazza@hotmail.com>
2022-01-29 12:49:58 +0100

    2.10i replaced authentication // Passport for get.movies

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.get("/documentation", (req, res) => {
 
   //GET all movies
   app.get("/movies",
-    //passport.authenticate("jwt", { session: false }),
+    passport.authenticate("jwt", { session: false }),
     (req, res) => {
       Movies.find()
         .then((movies) => {
